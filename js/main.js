5c2cc15e0c3681d91fa4152ac8dcb042ac6b779a
libepe <liam.beus@gmail.com>
2025-01-21 15:01:44 +0100

    mod 12

--- js/main.js
+++ js/main.js
@@ -1,33 +1,15 @@
-function scrollToMormon(){
-    const libromormon=document.getElementById('seccion-libromormon');
-    libromormon.scrollIntoView({
+//LA IGLESIA RESTAURADA
+
+
+function goDiv(id){
+    const div=document.getElementById(id);
+    div.scrollIntoView({
         block: 'start',
         behavior: 'smooth',
         inline: 'nearest',
     });
 };
 
-function scrollToJoseSmith(){
-    const josesmith=document.getElementById('seccion-josesmith');
-    josesmith.scrollIntoView({
-        block: 'start',
-        behavior: 'smooth',
-        inline: 'nearest',
-    });
-};
 
-function scrollToApostasia(){
-    const apostasia=document.getElementById('seccion-apostasia');
-    apostasia.scrollIntoView({
-        block: 'start',
-        behavior: 'smooth',
-        inline: 'nearest',
-    });
-};
 
-function goPage(){
-    const restauracion=document.querySelector('.pagina-restauracion');
-    restauracion.addEventListener('click', () => {
-        window.location.href="restauracion.html";
-    })
-}
+
